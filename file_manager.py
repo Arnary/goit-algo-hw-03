ecf670555751197c31bdeba79980bee31c17adf2
Arnary <anastasia.dushka1@gmail.com>
2024-01-10 00:12:14 +0200

    add argparse

--- file_manager.py
+++ file_manager.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import shutil
+import argparse
 
 
 COLOR_BLUE = "\u001b[34m"
@@ -7,19 +8,7 @@ COLOR_GREEN = "\u001b[32m"
 COLOR_RED = "\u001b[31m"
 
 
-def pars_input(user_input1, user_input2):
-    source_path = user_input1
-    destination_path = user_input2
-    return source_path, destination_path
-
-
 def file_sorter(source_path, destination_path):
-    source_path = Path(source_path)
-    if destination_path == "":
-        destination_path = Path(source_path/"dist")
-    else:
-        destination_path = Path(destination_path)
-
     for child in source_path.iterdir(): 
         if child.is_dir():
             file_sorter(child, destination_path)
@@ -40,11 +29,16 @@ def copy_file(file, destination_path):
 
 
 def main():
-    user_input1 = input("Enter the source path: ")
-    user_input2 = input("Enter the destination path(optional): ")
-    source_path, destination_path = pars_input(user_input1, user_input2)
+    parser = argparse.ArgumentParser(description="Copy and organize files based on file extensions.")
+    parser.add_argument("source_path", type=Path, help="Source directory path")
+    parser.add_argument("destination_path", nargs="?", type=Path, default=None, help="Destination directory path (default: 'dist' in the source directory)")
+    args = parser.parse_args()
+
+    if args.destination_path is None:
+        args.destination_path = args.source_path / "dist"
+
     try:
-        file_sorter(source_path, destination_path)
+        file_sorter(args.source_path, args.destination_path)
         print(COLOR_GREEN + "Sorting files into folders has been completed successfully.")
     except Exception as e:
         print(COLOR_RED + f"Error: {e}")
